95ceb4b28e62b5a20c442a5038f1c99167ae1ef6
Dalton Otineru <d@dmbp.local>
2021-10-21 12:09:18 -0700

    initial commit

--- functions-problems/problem1.js
+++ functions-problems/problem1.js
@@ -2,9 +2,10 @@
 step 1: define a javascript function that accepts a string as a parameter;
 step 2: split the string passed in, turn it into an array of individual words &
 store it in a variable;
-step 3: define a variable called longestWord and set the initial value to 0;
-step 4:
-step 5:
+step 3: define a variable called longestWord and set the initial value to an empty string ' ';
+step 4: start looping through your array of strings, so the longest word is set as the 
+value of longestWord variable;
+step 5: return the longestWord variable;
 */
 
  
@@ -23,3 +24,5 @@ function findLongestWord(str) {
 
 
 console.log(findLongestWord('Using Roots Technology to become a professional developer.'));
+
+// 'professional'
