be8b7e0fdeb738041b31e6d476ba73b2a1a37fba
Dalton Otineru <d@dmbp.local>
2021-10-22 22:31:22 -0700

    problem 4

---
+++ functions-problems/problem4.js
@@ -0,0 +1,23 @@
+/*
+step 1: Define a function that accepts a string as a parameter;
+step 2: Define a variable that splits that string that is being passed in, into an array of individual letters;
+step 3: Define a variable that reverses the string passed in;
+step 4: Define a variable that joins the reversed array back into a single word;
+step 5: Create an if, else statement that returns either true or false if the string is a palindrome;
+*/
+
+
+function palindrome(str) {
+  const arrayLetters = str.split('');
+  const reverseArray = arrayLetters.reverse();
+  const reverseString = arrayLetters.join('');
+  if(str.toLowerCase() === reverseString.toLowerCase()) {
+    return true;
+  } else { return false; }
+    
+}
+
+console.log(palindrome('RAceCar')); 
+// true
+  
+  
